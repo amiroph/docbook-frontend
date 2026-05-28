d4a58fa10c101d0247fe09bb0f24e34f6ceb6f0e
Amanuel <amannice77@gmail.com>
2026-05-28 14:52:24 +0300

    Force fix API URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,15 +1,4 @@
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
-});
-
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-export default API;
+import axios from "axios"; 
+const API = axios.create({ baseURL: "https://docbook-backend-1.onrender.com/api" }); 
+API.interceptors.request.use((config) => { const token = localStorage.getItem("token"); if (token) { config.headers.Authorization = "Bearer " + token; } return config; }); 
+export default API; 
